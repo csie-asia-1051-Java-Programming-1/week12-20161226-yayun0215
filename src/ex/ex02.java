42be8d829d321239e4f2f44dae752a0fd4541f9a
Yi-Cheng Chen <ChadCYB@users.noreply.github.com>
2016-12-26 14:15:05 +0800

    week12 init

---
+++ src/ex/ex02.java
@@ -0,0 +1,15 @@
+﻿package ex;
+/*
+ * Topic: 給定一字串，請將此字串中，每個字元的 ASCII 編碼數值進行加總，輸出其編碼數 值總和。舉例來說，若輸入之字串為 “abcdefghij” 共 10 個字元，其 ASCII 編碼依序為 “97 98 99 100 101 102 103 104 105 106” ，則編碼總和輸出為 1015 。
+ * Date: 2016/12/26
+ * Author: 1050210XX 周永振老師
+ */
+
+public class ex02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ex/ex03.java
@@ -0,0 +1,15 @@
+﻿package ex;
+/*
+ * Topic: OX 遊戲是大家小時候的童年記憶，甚至在長大後，無聊時還是會玩一下呢 ! 但當你一個人時，是否也能讓電腦跟你玩呢 ?
+ * Date: 2016/12/26
+ * Author: 1050210XX 周永振老師
+ */
+
+public class ex03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
