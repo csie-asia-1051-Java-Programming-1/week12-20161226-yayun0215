585723557b672c66d9833e855924e0a0707c84b5
yayun0215 <105021062@live.asia.edu.tw>
2017-01-03 22:10:10 +0800

    1

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,9 +1,12 @@
 package hw;
+import java.util.Scanner;
 
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		
 
 	}
 
--- src/hw/hw03.java
+++ src/hw/hw03.java
@@ -1,10 +1,32 @@
 package hw;
+import java.util.Scanner;
 
 public class hw03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();        
+        String data[] = str.split(" ");
+        for(int i=0;i<data.length; i++){
+        	
+        	
+        	data[i]=data[i].toLowerCase();
+        }
+        int j=0;
+        System.out.print(data[0] + " ");
+        for (int i =1;i<data.length;i++) {
+        	for (j = i-1;j>=0;j--) {
+        		if (data[j].equals(data[i]))
+        			break;
+        		}
+                if (j == -1) {
+                    System.out.print(data[i]+" ");
+                }
+            }
+            System.out.println();
+        }	
 
 	}
 
-}
+
